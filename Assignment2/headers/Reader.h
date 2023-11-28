35017ac3c268a0931bac8e3a42fb492925570cad
ashishkamblecctech <149309502+ashishkamblecctech@users.noreply.github.com>
2023-11-28 20:33:27 +0530

    Add files via upload

---
+++ Assignment2/headers/Reader.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Triangle.h"
+
+class Reader {
+public:
+    Reader(std::string filePath);
+
+    ~Reader();
+
+    void getTriangles(std::vector<Triangle>& triangles);
+
+private:
+    std::string mFilePath;
+    std::string isFileAvailable(); 
+};
